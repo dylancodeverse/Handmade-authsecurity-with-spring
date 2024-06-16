d12702222f747ac7c86596f391d49f041d97648e
dylancodeverse <ratianarivodylan@gmail.com>
2024-06-16 17:23:32 +0300

    maj

--- src/main/java/scaffold/framework/demo/config/springAuth/filters/AuthFilter.java
+++ src/main/java/scaffold/framework/demo/config/springAuth/filters/AuthFilter.java
@@ -22,7 +22,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import scaffold.framework.demo.config.springAuth.annotations.Auth;
 
-@WebFilter("/*")
 @Component
 public class AuthFilter implements Filter {
 
